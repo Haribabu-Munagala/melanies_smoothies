68419c64951d3eed771b567c8b3a60e42757173a
Haribabu-Munagala <Haribabu_Munagala@epam.com>
2025-09-19 21:52:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,9 +38,11 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
-
-
-
+#new 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response)
+sf_df = st.dataframe(data = smoothiefroot_response.json(),use_contianer_width=True)
 
 
 

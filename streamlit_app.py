91c15604683ef612652b9775767fd87ecc0829a5
Haribabu-Munagala <Haribabu_Munagala@epam.com>
2025-09-19 22:01:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width=True)
 
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -40,9 +42,7 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered,{name_on_order}!', icon="✅")
 #new 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response)
-sf_df = st.dataframe(data = smoothiefroot_response.json(),use_container_width=True)
+
 
 
 
